5423b404ee29fdbe4c4c070c94c73570ffa8861c
aymeric.henouille@gmail.com <aymeric.henouille.etu@univ-lille.fr>
2019-10-09 20:22:50 +0200

    feat: firebase
    - plugin firebase to the project
    - init firebase environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCUbM3STwT6pq6vGDBBdiwH6Ni2blw-lqc",
+    authDomain: "la-chopine-du-coin.firebaseapp.com",
+    databaseURL: "https://la-chopine-du-coin.firebaseio.com",
+    projectId: "la-chopine-du-coin",
+    storageBucket: "la-chopine-du-coin.appspot.com",
+    messagingSenderId: "665310998796",
+    appId: "1:665310998796:web:1efd500c0b7fa487575929",
+    measurementId: "G-55L4573XR4"
+  }
 };
 
 /*
