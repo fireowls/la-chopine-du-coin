8ead4bf1ff32476ebc1c37494ba00faab27381c4
aymeric.henouille@gmail.com <aymeric.henouille.etu@univ-lille.fr>
2019-10-11 16:07:07 +0200

    feat: basic register form

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { MaterialModule } from '../material/material.module';
 
 
 @NgModule({
-  declarations: [ HomeComponent ],
-  imports: [ CommonModule ],
+  declarations: [ HomeComponent, RegisterComponent ],
+  imports: [ CommonModule, MaterialModule ],
   exports: [ HomeComponent ]
 })
 export class HomeModule { }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 const material = [
   MatButtonModule,
-  MatToolbarModule
+  MatToolbarModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSelectModule
 ];
 
 @NgModule({
