f8d4410b0c4d1ddac7caf4509f49f30bfcbacae1
TheTeKsa <aymeric03d@gmail.com>
2019-10-25 11:19:27 +0200

    feat: profile
    add some tests in user.model.spec.ts

--- src/app/login/user/user.model.spec.ts
+++ src/app/login/user/user.model.spec.ts
@@ -28,4 +28,30 @@ describe('test the user model', () => {
         expect(user.nbImage).toBe(0);
     });
 
+    it('change firstName', () => {
+        user.firstName = 'bernard';
+        expect(user.firstName).toBe('bernard');
+    });
+
+    it('change lastName', () => {
+        user.lastName = 'lapin';
+        expect(user.lastName).toBe('lapin');
+    });
+
+    it('change birthday', () => {
+        const tmp = new Date('1999-07-20T00:00:00');
+        user.birthday = tmp;
+        expect(user.birthday).toBe(tmp);
+    });
+
+    it('change sex', () => {
+        user.sex = SexType.CHOPINE;
+        expect(user.sex).toBe(SexType.CHOPINE);
+    });
+
+    it('age', () => {
+        const tmp = new Date('2000-07-20T00:00:00');
+        expect(user.age).toBe(Math.trunc((Date.now().valueOf() - tmp.valueOf()) / 1000 / 60 / 60 / 24 / 365));
+    });
+
 });
